efc89a15785511f4114cd5e768dd640b08e09d7b
fabiosimplis <fabio.simplis@gmail.com>
2024-05-09 18:42:57 -0300

    Operações Basicas em List

---
+++ src/list/OperaçõesBasicas/ListaTarefa.java
@@ -0,0 +1,47 @@
+package list.OperaçõesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionaTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removeTarefas(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa tarefa : tarefaList){
+            if (tarefa.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(tarefa);
+            }
+        }
+
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public void removeUmaTarefa(String descricao){
+        for (Tarefa tarefa : tarefaList){
+            if (tarefa.getDescricao().equalsIgnoreCase(descricao)){
+                tarefaList.remove(tarefa);
+                break;
+            }
+        }
+    }
+
+    public int obterNumeroTotalDeTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescicoesTarefas(){
+        System.out.println(tarefaList);
+
+    }
+
+}
